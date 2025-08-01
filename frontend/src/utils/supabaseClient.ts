5b798cc9a6e8064b165944abe0ac3873b2c8d9df
ssandeep9o1 <sadeepkumar22@ifheindia.org>
2025-08-01 23:48:23 +0530

    removed supabaseclient from utils

--- frontend/src/utils/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
-
-// It's good practice to ensure your env variables are strings
-const supabaseUrl: string = process.env.VITE_SUPABASE_URL || '';
-const supabaseAnonKey: string = process.env.VITE_SUPABASE_ANON_KEY || '';
-
-export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
